4c71f639f56929a4226c42b83ecb3540dcac6ace
Arthur Ferreira <arthur_f.oliveira@hotmail.com>
2021-06-22 00:17:02 -0300

    feat: estruturação do banco
    criação das migrations
    criação da entidade user

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,9 @@
+import "reflect-metadata";
 import express from "express";
 
+import "./database";
+
 const app = express();
-
-app.get("/test", (req, res) => {
-  return res.send("Eae caray");
-});
-
-app.post("/test-post", (req, res) => {
-  return res.send("Eae post");
-});
 
 // Server running port
 app.listen(3000, () => console.log("Server is running..."));
